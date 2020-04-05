b973297157577e9815a7680160cc64113edf12ff
kristibui <kristi.bui27@gmail.com>
2020-04-05 19:47:15 -0400

    working profile for now

--- src/profile/ProfileScreenComponent.js
+++ src/profile/ProfileScreenComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './profile.scss';
-import {findAllUsers, findUserById, updateUser} from "../services/ProfileService";
+import {findUserById, updateUser} from "../services/ProfileService";
 
 class ProfileScreenComponent extends React.Component {
 
@@ -8,35 +8,13 @@ class ProfileScreenComponent extends React.Component {
         userFirstName: '',
         userLastName: '',
         userAge: '',
-        imgUrl: 'https://cdn.hswstatic.com/gif/hamster-alone.jpg',
 
-        // hardcode for now until API is working
-        // user: {}
-        user: {
-            "updated_at": "2020-04-05T02:31:45",
-            "polls": {
-              "__owner__": "Elixir.Polls.User",
-              "__field__": "polls",
-              "__cardinality__": "many"
-            },
-            "lastName": "Bui",
-            "inserted_at": "2020-04-05T02:31:45",
-            "id": "e9a52ecc-774d-43db-846b-da03cf63381e",
-            "firstName": "Kristi",
-            "comments": {
-              "__owner__": "Elixir.Polls.User",
-              "__field__": "comments",
-              "__cardinality__": "many"
-            },
-            "age": 21,
-            "__meta__": {
-              "state": "loaded",
-              "source": "users",
-              "schema": "Elixir.Polls.User",
-              "prefix": null,
-              "context": null
-            }
-          }
+        // HARDCODE - need to fix in user model
+        imgUrl: 'https://cdn.hswstatic.com/gif/hamster-alone.jpg',
+        userEmail: 'kristi.bui27@gmail.com',
+        userPassword: 'hamster_peace_sign_123.jpg',
+
+        user: {}
     }
 
     /**
@@ -52,7 +30,10 @@ class ProfileScreenComponent extends React.Component {
     componentDidMount = async () => {
         const user = await findUserById(this.props.userId)
         this.setState({
-            user: user
+            user: user,
+            userFirstName: user.firstName,
+            userLastName: user.lastName,
+            userAge: user.age,
         })
     }
 
@@ -60,57 +41,78 @@ class ProfileScreenComponent extends React.Component {
         return(
 
             <div>
-                <nav class='nav polls-nav'>
-                <a class="navbar-brand">NEU Polls</a>
-                <a class='nav-link' href="/">Home</a>
-                <a class="nav-link" href="/profile">Profile</a>
-                <a class="nav-link" href="/profile">Users</a>
-                <a class="nav-link" href="">Logout</a>
-            </nav>
+                <nav className='nav polls-nav'>
+                    <a className="navbar-brand" href="/">NEU Polls</a>
+                    <a className='nav-link' href="/">Home</a>
+                    <a className="nav-link" href="/profile">Profile</a>
+                    <a className="nav-link" href="/profile">Users</a>
+                    <a className="nav-link" href="/">Logout</a>
+                </nav>
 
             <div className='container-fluid'>
 
-                <div class='row whole-page'>
-
+                <div className='row whole-page'>
                     <div className='col-3 user-profile-left'>
-                        <h2 class='profile-header'>User Profile</h2>
+                        <h2 className='profile-header'>User Profile</h2>
+
+                        <hr class='line-break'></hr>
+
                         <ul>
-                            <li class='user-data-link'><a href=''>Your Polls</a></li>
-                            <li class='user-data-link'><a href=''>Your Comments</a></li>
+                            <li className='user-data-link'><a href='/'>Your Polls</a></li>
+                            <li className='user-data-link'><a href='/'>Your Comments</a></li>
                         </ul>
                     </div>
 
-                    <div className='col-9 user-profile-right'>
+                    <div className='col-9'>
+                        <form>
+                            <div class='row d-flex justify-content-center user-profile-right'>
 
-                    <form>
-                        <div class='form-group'>
+                                <div className='avatar profile-whitespace'>
+                                    <img className='profile-pic' 
+                                        src={this.state.imgUrl}
+                                        alt={this.state.user.firstName + "'s avatar"}/>
 
-                            <div class='row d-flex justify-content-center'>
-
-                                <div class='profile-whitespace'>
-                                    <div class='col-3'>
-                                        <div class='avatar profile-whitespace'>
-                                            <img class='profile-pic' 
-                                                src={this.state.imgUrl}/>
-
-                                            <div class="overlay">
-                                                <span class='edit-avatar'>
-                                                    <a href=''>Edit Avatar</a>
-                                                </span>
-                                            </div>
-                                        </div>
+                                    <div className="overlay">
+                                        <span className='edit-avatar'>
+                                            <a href='/'>Edit Avatar</a>
+                                        </span>
                                     </div>
                                 </div>
 
-                                <div class='col-7 user-header profile-whitespace'>
-                                    <span class='username'>
-                                        {this.state.user.firstName + ' ' + this.state.user.lastName}
-                                    </span>
+                                <span className='username'>
+                                    {this.state.user.firstName + ' ' + this.state.user.lastName}
+                                </span>
+
+                            </div>
+
+                            <div class='row d-flex justify-content-center'>
+                                <div className='col-sm-5 profile-whitespace'>
+                                    <label htmlFor="email">Email Address</label>
+                                    <input className="form-control" id="email" 
+                                            onChange={(e) => {
+                                                this.setState({
+                                                    userEmail: e.target.value
+                                                })
+                                            }}
+                                            placeholder={this.state.userEmail} />
                                 </div>
 
-                                <div class='col-5 profile-whitespace'>
-                                    <label for="first_name">First Name</label>
-                                    <input class="form-control" id="first_name" 
+                                <div className='col-sm-5 profile-whitespace'>
+                                    <label htmlFor="password">Password</label>
+                                    <input className="form-control" id="password"
+                                            onChange={(e) => {
+                                                this.setState({
+                                                    userPassword: e.target.value
+                                                })
+                                            }}
+                                            placeholder={this.state.userPassword} />
+                                </div>
+                            </div>
+
+                            <div class='row d-flex justify-content-center'>
+                                <div className='col-sm-5 profile-whitespace'>
+                                    <label htmlFor="first_name">First Name</label>
+                                    <input className="form-control" id="first_name" 
                                             onChange={(e) => {
                                                 this.setState({
                                                     userFirstName: e.target.value
@@ -119,9 +121,9 @@ class ProfileScreenComponent extends React.Component {
                                             placeholder={this.state.user.firstName} />
                                 </div>
 
-                                <div class='col-5 profile-whitespace'>
-                                    <label for="last_name">Last Name</label>
-                                    <input class="form-control" id="last_name"
+                                <div className='col-sm-5 profile-whitespace'>
+                                    <label htmlFor="last_name">Last Name</label>
+                                    <input className="form-control" id="last_name"
                                             onChange={(e) => {
                                                 this.setState({
                                                     userLastName: e.target.value
@@ -129,9 +131,12 @@ class ProfileScreenComponent extends React.Component {
                                             }}
                                             placeholder={this.state.user.lastName} />
                                 </div>
-                                <div class='col-5 profile-whitespace'>
-                                    <label for="age">Age</label>
-                                    <input class="form-control" id="age" 
+                            </div>
+
+                            <div class='row d-flex justify-content-center'>
+                                <div className='col-sm-5 profile-whitespace'>
+                                    <label htmlFor="age">Age</label>
+                                    <input className="form-control" id="age" 
                                             onChange={(e) => {
                                                 this.setState({
                                                     userAge: e.target.value
@@ -140,35 +145,27 @@ class ProfileScreenComponent extends React.Component {
                                             placeholder={this.state.user.age} />
                                 </div>
 
-                                <div class='col-5 profile-whitespace'>
-                                    <label for="phone_number">Role</label>
-                                    <input class="form-control" id="phone_number" placeholder="Admin" /> 
+                                <div className='col-sm-5 profile-whitespace'>
+                                    <label htmlFor="phone_number">Role</label>
+                                    <input className="form-control" id="phone_number" placeholder="Admin" /> 
                                 </div>
-
-                                <div class='col-5'>
-                                    <button type='button'
-                                            class='update-profile-btn shadowed p-2 ' 
-                                            onClick={(prevState) => {
-                                                        const updatedUser = {...prevState.user, 
-                                                                                age: this.state.userAge,
-                                                                                firstName: this.state.userFirstName,
-                                                                                lastName: this.state.userLastName};
-                                                        console.log(updatedUser)
-                                                        this.updateUser(updatedUser)}
-                                                    }>Save Changes</button>
-                                </div>
-                                <div class='col-5'></div>
                             </div>
-                        </div>
 
-                    </form>
-                </div>
-
+                            <div class='d-flex justify-content-center'>
+                            <button type='button'
+                                    className='update-profile-btn shadowed p-2' 
+                                    onClick={() => {
+                                            const updatedUser = {...this.state.user, 
+                                                                    'age': this.state.userAge,
+                                                                    'firstName': this.state.userFirstName,
+                                                                    'lastName': this.state.userLastName};
+                                            this.updateUser(updatedUser)}}>Save Changes</button>
+                            </div>
+                        </form>
+                    </div>
+                </div> 
             </div>
-
-
         </div>
-    </div>
 
         )
     }
