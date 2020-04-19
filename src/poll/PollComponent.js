e48a8c463da1c567c8ac04fa04c973f8cf0fcb7b
John Philip <johnphilip283@gmail.com>
2020-04-19 11:47:04 -0400

    adding poll components

--- src/poll/PollComponent.js
+++ src/poll/PollComponent.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './poll.scss';
+import { withRouter } from 'react-router-dom';
 
-const Option = ({ option }) => {
+const Option = ({ option, optionHandler }) => {
     return (
         <div class="option">
-            <input type="checkbox" className="mr-2" id={option}/>
-            <label class="question" for={option}>{option}</label>
+             <input type="radio" onClick={optionHandler} className="mr-2" id={option} name="option" value={option}/>
+            <label for={option}>{option}</label>
         </div>
     );
 }
 
-const PollComponent = ({ poll }) => {
+const PollComponent = ({ poll, history }) => {
+
+    const jwt = window.localStorage.getItem('token') || '';
+
+    const [selectedOption, setSelectedOption] = useState('');
+    
+    const { firstName, lastName, role } = poll.author;
+
+    const optionHandler = e => setSelectedOption(e.target.value);
+
+    const viewDetailsHandler = () => history.push(jwt ? `/polls/${poll.id}` : `/login`);
+    
     return (
-        <div class="poll">
-            <div class="inner-content">
-                <p class="m4">{poll.text}</p>
-                <div class="options">
-                    {poll.options.map(option => <Option option={option}/>)}
+        <div className="poll m-3">
+            <div className="inner-content p-4">
+                <p className='poll-author-name mb-0'>{`${firstName ? firstName + ' ' + lastName : 'Anonymous'}`}</p>
+                <div className="poll-byline mt-1">
+                    <span className="poll-role mr-1">{(role === 'user' || !role) ? 'Member' : 'Admin'}</span>-
+                    <span className='poll-time ml-1'>{new Date(poll.inserted_at).toDateString()}</span>
                 </div>
+                <p className='poll-question-text mt-2 mb-2'>{poll.text}</p>
+                <form className="options">
+                    {poll.options.map((option, idx) => <Option optionHandler={optionHandler} key={idx} option={option}/>)}
+                </form>
             </div>
+            <button className="details-link" onClick={viewDetailsHandler}>
+                View details
+            </button>
         </div>
     );
 }
 
-export default PollComponent;
+export default withRouter(PollComponent);
